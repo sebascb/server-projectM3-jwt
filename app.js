b1710862813123b6bb199aafdaa7ae7369942609
Sebas Corral <sebascorralbrito@gmail.com>
2022-02-26 13:05:15 +0100

    added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv/config");
 require("./db");
 const express = require("express");
+const cors = require('cors');
 
 const { isAuthenticated } = require("./middleware/jwt.middleware");
 const allRoutes = require("./routes");
@@ -11,6 +12,7 @@ const app = express();
 
 require("./config")(app);
 
+app.use(cors({ [origin: process.env.ORIGIN || 'http://localhost:3000' }));
 app.use("/api", allRoutes);
 app.use("/api/protected", isAuthenticated, protectedRoute);
 app.use("/auth", authRouter);
