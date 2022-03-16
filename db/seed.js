66a8f0806349a491d3a381f57b005fd5d9690085
Sebas Corral <sebascorralbrito@gmail.com>
2022-03-16 23:39:50 +0100

    seed extended

--- db/seed.js
+++ db/seed.js
@@ -4,7 +4,7 @@ const Card = require('../models/Card.model');
 const User = require('../models/User.model');
 const cards = [
   {
-    "image": "https://img.pokemondb.net/artwork/large/charmander.jpg",
+    "image": "https://img.pokemondb.net/sprites/home/normal/charmander.png",
     "name": "Charmander",
     "element": "Fire",
     "description": "Prefers hot things. They say that when it rains steam comes out of the tip of its tail.",
@@ -13,7 +13,7 @@ const cards = [
     "ability": "Flames Sea"
   },
   {
-    "image": "https://img.pokemondb.net/artwork/large/pikachu.jpg",
+    "image": "https://img.pokemondb.net/sprites/home/normal/pikachu.png",
     "name": "Pikachu",
     "element": "Electric",
     "description": "When several of these POKKéMON gather, their electricity could build and cause lightning storms.",
@@ -22,7 +22,7 @@ const cards = [
     "ability": "Static"
   },
   {
-    "image": "https://img.pokemondb.net/artwork/large/zubat.jpg",
+    "image": "https://img.pokemondb.net/sprites/go/normal/zubat.png",
     "name": "Zubat",
     "element": "Poison",
     "description": "Forms colonies in perpetually dark places. Uses ultrasonic waves to identify and approach targets.",
@@ -31,7 +31,7 @@ const cards = [
     "ability": "Inner Focus"
   },
   {
-    "image": "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+    "image": "https://img.pokemondb.net/sprites/home/normal/bulbasaur.png",
     "name": "Bulbasaur",
     "element": "Grass",
     "description": "A strange seed was planted on its back at birth. The plant sprouts and grows with this POKKéMON.",
@@ -40,7 +40,7 @@ const cards = [
     "ability": "Overgrow"
   },
   {
-    "image": "https://img.pokemondb.net/artwork/large/squirtle.jpg",
+    "image": "https://img.pokemondb.net/sprites/home/normal/squirtle.png",
     "name": "Squirtle",
     "element": "Water",
     "description": "After birth, its back swells and hardens into a shell. Powerfully sprays foam from its mouth.",
@@ -49,7 +49,7 @@ const cards = [
     "ability": "Torrent"
   },
   {
-    "image": "https://img.pokemondb.net/artwork/large/weedle.jpg",
+    "image": "https://img.pokemondb.net/sprites/home/normal/weedle.png",
     "name": "Weedle",
     "element": "Poison",
     "description": "Often found in forests, eating leaves. It has a sharp venomous stinger on its head.",
@@ -58,7 +58,7 @@ const cards = [
     "ability": "Shield Dust"
   },
   {
-    "image": "https://img.pokemondb.net/artwork/large/rattata.jpg",
+    "image": "https://img.pokemondb.net/sprites/home/normal/rattata.png",
     "name": "Rattata",
     "element": "Normal",
     "description": "Bites anything when it attacks. Small and very quick, it is a common sight in many places.",
@@ -67,7 +67,7 @@ const cards = [
     "ability": "Run Away"
   },
   {
-    "image": "https://img.pokemondb.net/artwork/large/ekans.jpg",
+    "image": "https://img.pokemondb.net/sprites/home/normal/ekans.png",
     "name": "Ekans",
     "element": "Poison",
     "description": "Moves silently and stealthily. Eats the eggs of birds.",
@@ -76,8 +76,8 @@ const cards = [
     "ability": "Intimidate"
   },
   {
-    "image": "https://img.pokemondb.net/artwork/large/vulpix.jpg",
-    "name": "Pesterix",
+    "image": "https://img.pokemondb.net/sprites/home/normal/vulpix.png",
+    "name": "Vulpix",
     "element": "Fire",
     "description": "When she sleeps she is adorable but if you wake her up she transforms",
     "attack": 50,
@@ -85,7 +85,7 @@ const cards = [
     "ability": "Little Monster"
   },
   {
-    "image": "https://img.pokemondb.net/artwork/large/oddish.jpg",
+    "image": "https://img.pokemondb.net/sprites/home/normal/oddish.png",
     "name": "Oddish",
     "element": "Grass",
     "description": "During the day, it keeps its face buried in the ground. At night, it wanders around sowing its seeds.",
